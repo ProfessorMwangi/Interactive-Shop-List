7e2ad4b58e8feaad50eeb68f9118dd2dbfcfcf10
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-07-08 11:39:38 +0300

    clear button fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,41 @@
+/** @format */
+let clearBtn = document.getElementById("clear");
 
-let form = document.addEventListener('DOMContentLoaded',()=> {
-  document.getElementById('formBody')
-  let clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", deleteAll);
+let form = document.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("formBody");
+	clearBtn.addEventListener("click", deleteAll);
 
-  formBody.addEventListener('submit', (e) => {
-    e.preventDefault()
-    handleShopping(e.target.txt.value);
-    formBody.reset()
-  })
-})
+	formBody.addEventListener("submit", (e) => {
+		e.preventDefault();
+		console.log(e.target.txt.value);
+		handleShopping(e.target.txt.value);
+		formBody.reset();
+	});
+});
+
 clearBtn.addEventListener("click", (e) => {
-  e.preventDefault()
-  deleteAll()
-  p.style.display='none'
+	e.preventDefault();
+	deleteAll();
 });
 
 function handleShopping(shop) {
-  let p = document.createElement('p')
-  p.textContent = `${shop}  `
-  shopList.appendChild(p)
-  let btn = document.createElement('button')
-  let checkbox = document.createElement('input')
-  checkbox.type = 'checkbox'
- 
-  p.appendChild(checkbox)
-  btn.addEventListener('click',deleteOneList)
-  btn.textContent = 'Delete'
-  p.appendChild(btn)
-  
+	let p = document.createElement("p");
+	p.textContent = `${shop}  `;
+	shopList.appendChild(p);
+	let btn = document.createElement("button");
+	let checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+
+	p.appendChild(checkbox);
+	btn.addEventListener("click", deleteOneList);
+	btn.textContent = "Delete";
+	p.appendChild(btn);
 }
 function deleteOneList(e) {
-  e.target.parentNode.remove()
+	e.target.parentNode.remove();
 }
 
- 
 function deleteAll() {
-  let shopList = document.getElementById('shopList') 
-shopList.innerHTML = ''
-  shopList.remove(innerHTML)
+	let shopList = document.getElementById("shopList");
+	shopList.innerHTML = "";
 }
